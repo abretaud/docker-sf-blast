eb6292a299f5d28888ed95fb2b5308fd24b7123c
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2020-03-21 16:14:59 +0100

    dot notation

--- bin/blast_links.py
+++ bin/blast_links.py
@@ -161,8 +161,8 @@ class LinkInjector():
     def inject_link(self, db, line):
 
         # Url encoded json
-        jbrowse_track = "&addStores=%7B%22url%22%3A%7B%22type%22%3A%22JBrowse%2FStore%2FSeqFeature%2FGFF3%22%2C%22urlTemplate%22%3A%22" + self.args.gff_url + "%22%7D%7D"
-        jbrowse_track += "&addTracks=%5B%7B%22label%22%3A%22Blast results%22%2C%22type%22%3A%22JBrowse%2FView%2FTrack%2FHTMLFeatures%22%2C%22store%22%3A%22url%22%7D%5D"
+        jbrowse_track = "&addStores.blast.type=JBrowse%2FStore%2FSeqFeature%2FGFF3&addStores.blast.urlTemplate=" + self.args.gff_url + ""
+        jbrowse_track += "&addTracks.blast.label=Blast%20results&addTracks.blast.type=JBrowse%2FView%2FTrack%2FHTMLFeatures&addTracks.blast.store=blast"
         jbrowse_track += "&tracks=Blast%20results"
 
         # Apollo looks the same as JBrowse for now
